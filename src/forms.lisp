5313d851d851e780f82bdf0450bbf3eae0054e2d
NMunro <nmunro@duck.com>
2025-03-31 23:29:28 +0100

    Chapter 6

--- src/forms.lisp
+++ src/forms.lisp
@@ -2,19 +2,22 @@
   (:use :cl)
   (:export #:register
            #:email
+           #:username
            #:password
-           #:password-verify
-           #:*password-validator*))
+           #:password-verify))
 
 (in-package ningle-tutorial-project/forms)
+
+(defparameter *username-validator* (list (clavier:not-blank)
+                                         (clavier:is-a-string)))
 
 (defparameter *password-validator* (list (clavier:not-blank)
                                          (clavier:is-a-string)
                                          (clavier:len :min 8)))
 
 (cl-forms:defform register (:action "/register" :id "signup" :csrf-protection t :csrf-field-name "csrftoken")
-  ((email :email :value "")
-   (password :password :value "" :constraints *password-validator*)
+  ((email           :email    :value "" :constraints (list (clavier:valid-email)))
+   (username        :string   :value "" :constraints *username-validator*)
+   (password        :password :value "" :constraints *password-validator*)
    (password-verify :password :value "" :constraints *password-validator*)
-   (submit :submit :label "Register")))
-
+   (submit          :submit   :label "Register")))
