ff07cf732b04520235c28b9f8ed56442eca01a4d
NMunro <nmunro@duck.com>
2025-07-25 23:08:07 +0100

    Tidied up create-super-user

--- src/contrib.lisp
+++ src/contrib.lisp
@@ -6,6 +6,6 @@
 
 (defun create-super-user (&key username email password)
   (with-db-connection
-      (let* ((u (create-dao 'ningle-auth/models:user :username username :email email :password password))
-             (r (create-dao 'ningle-auth/models:role :name "admin" :description "Admin")))
-        (create-dao 'ningle-auth/models:permission :user u :role r))))
+      (let ((user (create-dao 'ningle-auth/models:user :username username :email email :password password :active 1)))
+        (create-dao 'ningle-auth/models:permission :user user :role (find-dao 'ningle-auth/models:role :name "admin"))
+        user)))
